754b748397380bbfb311d905f6f1cc09815b23d3
mr_maksis <max.morink@yandex.ru>
2023-06-10 17:04:04 +0300

    Homework_1

--- homework_1.py
+++ homework_1.py
@@ -1,1 +1,13 @@
+def count_vowels(word):
+    vowels = "аоуэыиеёяю"
+    return sum([1 for letter in word.lower() if letter in vowels])
 
+poem = input("Введите стихотворение: ")
+phrases = poem.split()
+
+vowel_counts = [count_vowels(phrase.replace('-', '')) for phrase in phrases]
+
+if len(set(vowel_counts)) == 1:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
